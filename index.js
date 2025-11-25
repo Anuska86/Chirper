ed959c448ad0a45be3151a3a04f141aaa4b7a269
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-11-25 12:05:30 +0100

    replies

--- index.js
+++ index.js
@@ -67,6 +67,24 @@ function getFeedHtml() {
       retweetClass = "retweeted";
     }
 
+    let repliesHtml = "";
+
+    if (tweet.replies.length > 0) {
+      tweet.replies.forEach(function (reply) {
+        repliesHtml += `
+<div class="tweet-reply">
+    <div class="tweet-inner">
+        <img src="${reply.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${reply.handle}</p>
+                <p class="tweet-text">${reply.tweetText}</p>
+            </div>
+        </div>
+</div>
+`;
+      });
+    }
+
     feedHtml += `<div class="tweet">
     <div class="tweet-inner">
         <img src="${tweet.profilePic}" class="profile-pic">
@@ -89,6 +107,9 @@ function getFeedHtml() {
             </div>   
         </div>            
     </div>
+    <div id="replies-${tweet.uuid}">
+          ${repliesHtml}
+    </div>   
 </div>`;
   });
 
